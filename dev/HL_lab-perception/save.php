4bfd4b80996937d3a664d527562149bbe977b05d
Hugo Ladret <hugo.ladret@gmail.com>
2022-11-30 14:50:45 +0100

    push save

--- dev/HL_lab-perception/save.php
+++ dev/HL_lab-perception/save.php
@@ -1,8 +1,14 @@
-if(!empty($_POST['data'])){
-$data = $_POST['data'];
-$fname = mktime() . ".txt";//generates random name
+<?php
+debug_to_console("test");
+$json = file_get_contents('php://input');
+file_put_contents('results.txt', $json, FILE_APPEND);
 
-$file = fopen("upload/" .$fname, 'w');//creates new file
-fwrite($file, $data);
-fclose($file);
+function debug_to_console($data) {
+   $output = $data;
+   if (is_array($output))
+	   $output = implode(',', $output);
+
+   echo "<script>console.log('Debug Objects: " . $output . "' );</script>";
 }
+
+?>
